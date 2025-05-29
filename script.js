a47a7ce954bf51f519bf666a2918d92d80f5e485
J. Pedro <144042764+pedromilanista@users.noreply.github.com>
2025-05-29 08:46:53 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Aqui você pode adicionar interatividade ao seu painel webview,
+    // manipulando os elementos HTML que foram criados no index.html.
+
+    // Exemplo: Adicionar um ouvinte de evento a um botão (se você criar um)
+    // const meuBotao = document.getElementById('meuBotao');
+    // if (meuBotao) {
+    //     meuBotao.addEventListener('click', () => {
+    //         alert('Botão clicado!');
+    //     });
+    // }
+
+    console.log('Script.js carregado!');
+});
